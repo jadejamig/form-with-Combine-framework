7dfe0c5dbc2a8f4b35072476e764ed48aca9c5dd
Jade Jamig <ajamig016@gmail.com>
2021-06-16 12:54:45 +0800

    Initial Commit

---
+++ FormWithCombine/FormWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormWithCombineApp.swift
+//  FormWithCombine
+//
+//  Created by Anselm Jade Jamig on 6/16/21.
+//
+
+import SwiftUI
+
+@main
+struct FormWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
